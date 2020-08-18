ea1017655cf344acbf297780ce9e91b2e5ec8ce0
gautam <gautam.sethi_cs19@gla.ac.in>
2020-08-18 20:03:45 +0530

    changed operator

--- src/gautam/PalindromeString.java
+++ src/gautam/PalindromeString.java
@@ -19,7 +19,7 @@ public class PalindromeString {
         original = scanner.nextLine();
         int length = original.length();
         for (int i = length - 1; i >= 0; i--) {
-            reverse = reverse + original.charAt(i);
+            reverse += original.charAt(i);
 
 
         }
